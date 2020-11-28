552fefbad7a16c309843afd17c48e70e894e016a
breno-do-val <brenorvale@gmail.com>
2020-11-28 11:27:26 -0300

    (tag): create tag for first version

--- app.js
+++ app.js
@@ -3,36 +3,33 @@ class App {
     console.log("Initialized...");
     this.bindButtonListener();
   }
-  
+
   bindButtonListener() {
     const button = document.querySelector("button");
     button.addEventListener("click", () => {
       this.saveToStorage();
     });
   }
-  
+
   saveToStorage() {
     const keyInput = document.getElementById("key");
     const valueInput = document.getElementById("value");
-    
-    if(keyInput.value && valueInput.value) {
-      localStorage.setItem(keyInput.value, valueInput.value)
+
+    if (keyInput.value && valueInput.value) {
+      localStorage.setItem(keyInput.value, valueInput.value);
       this.listStorageValues();
     }
   }
 
   listStorageValues() {
-    const storageValues = document.getElementById('storageValues');
+    const storageValues = document.getElementById("storageValues");
 
-    const toHtml = key => {
+    const toHtml = (key) => {
       const value = localStorage.getItem(key);
       return `<p>${key}: ${value}</p>`;
     };
 
-    const htmlOutput = Object.keys(localStorage)
-      .sort()
-      .map(toHtml)
-      .join('');
+    const htmlOutput = Object.keys(localStorage).sort().map(toHtml).join("");
 
     storageValues.innerHTML = htmlOutput;
   }
